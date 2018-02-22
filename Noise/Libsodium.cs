99bb78c51f9c0e752786d4c677962bc5cc6f5589
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-02-22 15:51:14 +0100

    Add libsodium ChaCha20-Poly1305 interop

---
+++ Noise/Libsodium.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Noise
+{
+	internal static class Libsodium
+	{
+		private const string Name = "libsodium";
+
+		public const int crypto_aead_chacha20poly1305_ietf_KEYBYTES = 32;
+		public const int crypto_aead_chacha20poly1305_ietf_NPUBBYTES = 12;
+		public const int crypto_aead_chacha20poly1305_ietf_ABYTES = 16;
+
+		static Libsodium()
+		{
+			if (sodium_init() == -1)
+			{
+				throw new CryptographicException("Failed to initialize libsodium.");
+			}
+		}
+
+		[DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
+		private static extern int sodium_init();
+
+		[DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int crypto_aead_chacha20poly1305_ietf_encrypt(
+			byte[] c,
+			out long clen_p,
+			byte[] m,
+			long mlen,
+			byte[] ad,
+			long adlen,
+			IntPtr nsec,
+			byte[] npub,
+			byte[] k
+		);
+
+		[DllImport(Name, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int crypto_aead_chacha20poly1305_ietf_decrypt(
+			byte[] m,
+			out long mlen_p,
+			IntPtr nsec,
+			byte[] c,
+			long clen,
+			byte[] ad,
+			long adlen,
+			byte[] npub,
+			byte[] k
+		);
+	}
+}
