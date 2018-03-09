a16a5f8c7c58af91ae5e26602a990fd1bb9a2042
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-03-09 19:21:42 +0100

    Remove CipherSuite class

--- Noise/Protocol.cs
+++ Noise/Protocol.cs
@@ -12,7 +12,9 @@ namespace Noise
 		/// cipher functions, DH functions, and hash functions.
 		/// </summary>
 		public static IHandshakeState Create(
-			CipherSuite cipherSuite,
+			CipherFunction cipher,
+			DhFunction dh,
+			HashFunction hash,
 			HandshakePattern handshakePattern,
 			bool initiator,
 			byte[] prologue,
@@ -20,33 +22,33 @@ namespace Noise
 			byte[] rs)
 		{
 
-			if (cipherSuite.Cipher == CipherFunction.AesGcm && cipherSuite.Hash == HashFunction.Sha256)
+			if (cipher == CipherFunction.AesGcm && hash == HashFunction.Sha256)
 			{
 				return new HandshakeState<Aes256Gcm, Curve25519, Sha256>(handshakePattern, initiator, prologue, s, rs);
 			}
-			else if (cipherSuite.Cipher == CipherFunction.AesGcm && cipherSuite.Hash == HashFunction.Sha512)
+			else if (cipher == CipherFunction.AesGcm && hash == HashFunction.Sha512)
 			{
 				return new HandshakeState<Aes256Gcm, Curve25519, Sha512>(handshakePattern, initiator, prologue, s, rs);
 			}
-			else if (cipherSuite.Cipher == CipherFunction.AesGcm && cipherSuite.Hash == HashFunction.Blake2b)
+			else if (cipher == CipherFunction.AesGcm && hash == HashFunction.Blake2b)
 			{
 				return new HandshakeState<Aes256Gcm, Curve25519, Blake2b>(handshakePattern, initiator, prologue, s, rs);
 			}
-			else if (cipherSuite.Cipher == CipherFunction.ChaChaPoly && cipherSuite.Hash == HashFunction.Sha256)
+			else if (cipher == CipherFunction.ChaChaPoly && hash == HashFunction.Sha256)
 			{
 				return new HandshakeState<ChaCha20Poly1305, Curve25519, Sha256>(handshakePattern, initiator, prologue, s, rs);
 			}
-			else if (cipherSuite.Cipher == CipherFunction.ChaChaPoly && cipherSuite.Hash == HashFunction.Sha512)
+			else if (cipher == CipherFunction.ChaChaPoly && hash == HashFunction.Sha512)
 			{
 				return new HandshakeState<ChaCha20Poly1305, Curve25519, Sha512>(handshakePattern, initiator, prologue, s, rs);
 			}
-			else if (cipherSuite.Cipher == CipherFunction.ChaChaPoly && cipherSuite.Hash == HashFunction.Blake2b)
+			else if (cipher == CipherFunction.ChaChaPoly && hash == HashFunction.Blake2b)
 			{
 				return new HandshakeState<ChaCha20Poly1305, Curve25519, Blake2b>(handshakePattern, initiator, prologue, s, rs);
 			}
 			else
 			{
-				throw new ArgumentException("Cipher suite not supported.", nameof(cipherSuite));
+				throw new ArgumentException("Cipher suite not supported.");
 			}
 		}
 
@@ -86,34 +88,32 @@ namespace Noise
 				return false;
 			}
 
-			DhFunction dhType;
-			CipherFunction cipherType;
-			HashFunction hashType;
+			DhFunction dh;
+			CipherFunction cipher;
+			HashFunction hash;
 
 			switch (parts[2])
 			{
-				case "25519": dhType = DhFunction.Curve25519; break;
+				case "25519": dh = DhFunction.Curve25519; break;
 				default: return false;
 			}
 
 			switch (parts[3])
 			{
-				case "AESGCM": cipherType = CipherFunction.AesGcm; break;
-				case "ChaChaPoly": cipherType = CipherFunction.ChaChaPoly; break;
+				case "AESGCM": cipher = CipherFunction.AesGcm; break;
+				case "ChaChaPoly": cipher = CipherFunction.ChaChaPoly; break;
 				default: return false;
 			}
 
 			switch (parts[4])
 			{
-				case "SHA256": hashType = HashFunction.Sha256; break;
-				case "SHA512": hashType = HashFunction.Sha512; break;
-				case "BLAKE2b": hashType = HashFunction.Blake2b; break;
+				case "SHA256": hash = HashFunction.Sha256; break;
+				case "SHA512": hash = HashFunction.Sha512; break;
+				case "BLAKE2b": hash = HashFunction.Blake2b; break;
 				default: return false;
 			}
 
-			CipherSuite cipherSuite = new CipherSuite(cipherType, dhType, hashType);
-			handshakeState = Create(cipherSuite, pattern, initiator, prologue, s, rs);
-
+			handshakeState = Create(cipher, dh, hash, pattern, initiator, prologue, s, rs);
 			return true;
 		}
 	}
